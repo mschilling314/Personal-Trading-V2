2df2e3728e726025a1e6ebefa1c8f9330809321b
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-10-02 18:03:19 -0500

    Update logger.py

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -6,9 +6,6 @@ def init_logger() -> logging.Logger:
     Initializes a logger for use that will write to logs/app.log.
     """
     logger = logging.getLogger(__name__)
-    logger.setLevel("Debug")
-    handler = logging.FileHandler("logs/app.log")
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    log_file_path = os.path.join(os.getcwd(), "logs", "app.log")
+    logging.basicConfig(filename=log_file_path, format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
     return logger
