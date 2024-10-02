10b8d4e9efe26d92f94fe7b8069f1c6de3d7ed32
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-10-02 08:10:17 -0500

    Script Logging

--- afternoon_trade.py
+++ afternoon_trade.py
@@ -16,27 +16,30 @@ from src.logger.logger import init_logger
 # intitialization
 ticker = os.environ["Ticker"]
 logger = init_logger()
+logger.info("Morning run started.")
 try:
     access_token = get_access_token()
 except:
-    logger.error("Error getting access token.")
+    logger.critical("Error getting access token.")
     exit()
 
 # retrieve account positions and transactions
 acct = get_account_positions(access_token=access_token)
+logger.debug(f"EoD account state:\n{acct}")
 money = acct["liquidity"]
 positions = acct["positions"]
-logger.info(f"EOD account state:\n{acct}")
 txns_today = get_transactions_from_today(access_token=access_token)
-logger.info(f"Today's Transactions:\n{txns_today}")
+logger.debug(f"Today's Transactions:\n{txns_today}")
 
 # if we have one transaction today, that means we have an open position we need to close
 if len(txns_today) == 1:
     logger.warn("Position left open.\n")
     for ticker, position in positions.items():
         response = place_market_order(access_token=access_token, quantity=position["quantity"], instruction="SELL", ticker=ticker)
+        logger.debug(f"Response from Schwab is:\n{response}")
         # TODO: Add a check here to make sure order goes thru?
     txns_today = get_transactions_from_today(access_token=access_token)
+    logger.debug(f"Updated transactions for today:\n{txns_today}")
     # may not need, since the orders expire at EOD anyway
     # orders = get_orders_from_today(access_token=access_token)
     # for order in orders:
@@ -47,7 +50,7 @@ if len(txns_today) == 1:
 df = pd.DataFrame(txns_today)
 conn = sqlite3.connect("trading_data.sqlite")
 df.to_sql("trading_data.sqlite", con=conn, if_exists="append")
-logger.info("Database updated.")
+logger.info("Afternoon run finished.")
 
 
 
--- morning_trade.py
+++ morning_trade.py
@@ -14,28 +14,32 @@ from src.logger.logger import init_logger
 # init
 ticker = os.environ["Ticker"]
 logger = init_logger()
+logger.info("Morning run started.")
 try:
     access_token = get_access_token()
 except:
-    logger.error("Error fetching access token.")
+    logger.critical("Error fetching access token.")
     exit()
 
 # First, we check to see how much money we have
-money = get_account_positions(access_token=access_token)["liquidity"]
+acct = get_account_positions(access_token=access_token)
+logger.debug(f"Account state at day start:\n{acct}")
+money = acct["liquidity"]
 # TODO: implement logic to decide if we want to buy or not, for now assume we always do
-# gets the current price, then calculates sale prices; for now, try setting limit order to be equiv to current loss_stop_price
+# gets the current price, then calculates sale prices
 curr_price = get_current_price()
 profit_capture_price = 1.025 * curr_price
 loss_stop_price = 0.99 * curr_price
-logger.info(f"The current price is {curr_price}.")
+loss_price = loss_stop_price - 0.01
+logger.debug(f"The current price is {curr_price}.  Spread has a high of {profit_capture_price} and low of {loss_stop_price}")
 
 # ordering time
 quantity = money / curr_price
 market_order_resp = place_market_order(access_token=access_token, quantity=quantity, instruction="BUY", ticker=ticker)
-logger.info(f"Bought {quantity} of {ticker}, here's the response:\n{market_order_resp}")
+logger.debug(f"Bought {quantity} of {ticker}, here's the response:\n{market_order_resp}")
 # TODO: make sure market order went thru
-oco_order_resp = place_oco_order(access_token=access_token, quantity=quantity, limit_price=profit_capture_price, stop_limit_price=loss_stop_price, stop_price=loss_stop_price, ticker=ticker)
-logger.info(f"Set OCO order, here's the response:\n{oco_order_resp}")
+oco_order_resp = place_oco_order(access_token=access_token, quantity=quantity, limit_price=profit_capture_price, stop_limit_price=loss_stop_price, stop_price=loss_price, ticker=ticker)
+logger.debug(f"Set OCO order, here's the response:\n{oco_order_resp}")
 # TODO: make sure OCO order is received
-
+logger.debug(f"Morning run complete.")
 
