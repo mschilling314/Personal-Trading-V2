f98e83b5e293f4bac752a6de7fb52082ec0abbe5
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-10-02 16:17:03 -0500

    Update account.py

--- src/schwab/account.py
+++ src/schwab/account.py
@@ -9,7 +9,7 @@ import logging
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename="logs/app.log", format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
+logging.basicConfig(filename="~/logs/app.log", format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
 
 
 def get_transactions_from_today(access_token) -> requests.Response:
