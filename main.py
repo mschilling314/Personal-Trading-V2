4589ad1e13fcda702496c41b471a7df84fa88d50
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-10-01 19:43:09 -0500

    Exp 2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
 from datetime import datetime
+import yfinance as yf
+import pandas as pd
 
 logging.basicConfig(filename="logs/main_log.log", format="%(asctime)s %(message)s", filemode="a")
 
@@ -7,3 +9,7 @@ logger = logging.getLogger()
 
 logger.setLevel(logging.INFO)
 logger.info(f"Time is now {datetime.now()}")
+
+yahoo = yf.download("TQQQ", period="1d", interval="1m")
+df = pd.DataFrame(yahoo)
+df.to_csv("trading_data.csv")
