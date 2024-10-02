f25df1b5ce769a2cc08383479a7f2daefebd4fb0
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-10-01 19:31:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import datetime.datetime as dt
 
 
-logging.basicConfig(filename="logs/main_log.log", format="%(asctime)s %(message)s" filemode="a")
+logging.basicConfig(filename="logs/main_log.log", format="%(asctime)s %(message)s", filemode="a")
 
 logger = logging.getLogger()
 
