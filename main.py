6c48f751a9fe06bf6e6cfb8c0e9a1a3156b8f868
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-10-02 16:47:22 -0500

    Revert "Update main.py"
    This reverts commit ca7650df9229024c692a987e5c9e5910d54f1bbe.

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from src.schwab.account import get_account_positions
 
 
 
-logging.basicConfig(filename="logs/main_log.log", format="%(asctime)s %(message)s", filemode="a")
+# logging.basicConfig(filename="logs/main_log.log", format="%(asctime)s %(message)s", filemode="a")
 
-logger = logging.getLogger()
+# logger = logging.getLogger()
 
-logger.setLevel(logging.INFO)
-logger.info(f"Time is now {datetime.now()}")
+# logger.setLevel(logging.INFO)
+# logger.info(f"Time is now {datetime.now()}")
 
-yahoo = yf.download("TQQQ", period="1d", interval="1m")
-df = pd.DataFrame(yahoo)
-conn = sqlite3.connect("trading_data.sqlite")
-df.to_sql(name="TQQQ", con=con, if_exists="append")
+# yahoo = yf.download("TQQQ", period="1d", interval="1m")
+# df = pd.DataFrame(yahoo)
+# conn = sqlite3.connect("trading_data.sqlite")
+# df.to_sql(name="TQQQ", con=con, if_exists="append")
