7e7694a1c006ddc7248f4d209f5a76b612f6d2bf
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-11-11 01:09:06 -0600

    Updates to make backtesting work

--- backtest/orchestrator.py
+++ backtest/orchestrator.py
@@ -8,23 +8,6 @@ from models.CoinFlip import CoinFlip
 SCHEMA = {"Open": float, "High": float, "Low": float, "Close": float, "Volume": float}
 
 
-def execute_backtest(data, strat, ticker: str="TQQQ"):
-    bt = backtesting.Backtest(data=data, strategy=strat, exclusive_orders=True, cash=10000)
-    stats = bt.run()
-    results_file_name = "results/" + ticker + f"{strat.__name__}_results.csv"
-    trades_file_name = "trades/" + ticker + f"{strat.__name__}_trades.csv"
-    stats.to_csv(results_file_name)
-    stats["_trades"].to_csv(trades_file_name)
-    print("\Test finished.\n")
-    graph_file_name = "graphs/" + ticker + f"{strat.__name__}_Vol.html"
-    # bt.plot(filename=graph_file_name)
-
-def execute_backtest_v2(data, strat, start, end, ticker: str="TQQQ", money=10000):
-    bt = backtesting.Backtest(data=data, strategy=strat, exclusive_orders=True, cash=money)
-    stats = bt.run()
-    return stats["Equity Final [$]"]
-
-
 def orchestrate(data_specs: dict, model: backtesting.Strategy, results_directory: str="./backtest/results/") -> None:
     """
     Create a new backtesting flow.  Flow should go:
@@ -51,11 +34,11 @@ def orchestrate(data_specs: dict, model: backtesting.Strategy, results_directory
         stats = bt.run()
         stats.to_csv(f"{results_directory}{model.__name__}_results.csv")
         stats.to_csv(f"{results_directory}{model.__name__}_trades.csv")
-        bt.plot(filename=f"{results_directory}{model.__name__}_graphs.html", open_browser=False)
+        # bt.plot(filename=f"{results_directory}{model.__name__}_graphs.html", open_browser=False)
         print("Backtesting has concluded.")
 
 
 
 if __name__=="__main__":
-    data_specs = {"loader": load_data_from_alpha_vantage, "ticker": "TQQQ", "interval": "1min", "start_date": datetime.date(2020,1,1), "end_date": datetime.date(2024,10,1)}
+    data_specs = {"loader": load_data_from_alpha_vantage, "ticker": "TQQQ", "interval": "1min", "start_date": datetime.date(2024,1,1), "end_date": datetime.date(2024,10,1)}
     orchestrate(data_specs=data_specs, model=CoinFlip)
